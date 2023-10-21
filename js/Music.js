c720c446e6b25116c143a6802e165d5d57e104ff
Michael Silva <michael.sigma.3@gmail.com>
2023-10-21 11:04:07 -0300

    Adicionando os acrodes menores

--- js/Music.js
+++ js/Music.js
@@ -10,7 +10,19 @@ const ACORDES = {
 	Ab: ['G#3', 'C4', 'D#4'],
 	A: ['A3', 'C#4', 'E4'],
 	Bb: ['A#3', 'D4', 'F4'],
-	B: ['B3', 'E4', 'G#4']
+	B: ['B3', 'D#4', 'F#4'],
+	Cm: ['C3', 'D#3', 'G3'],
+	Dbm: ['C#3', 'E3', 'G#3'],
+	Dm: ['D3', 'F3', 'A3'],
+	Ebm: ['D#3', 'F#3', 'A#3'],
+	Em: ['E3', 'G3', 'B3'],
+	Fm: ['F3', 'G#3', 'C4'],
+	Gbm: ['F#3', 'A3', 'C#4'],
+	Gm: ['G3', 'A#3', 'D4'],
+	Abm: ['G#3', 'B3', 'D#4'],
+	Am: ['A3', 'C4', 'E4'],
+	Bbm: ['A#3', 'C#4', 'F4'],
+	Bm: ['B3', 'D4', 'F#4']
 };
 
 function sleep(ms) {
@@ -32,13 +44,12 @@ async function IniciarReproducao(acorde) {
 
 function ReproduzirAcorde(Acorde) {
 	var notas = ObterAcorde(Acorde);
-	synth.triggerAttackRelease(notas, [0.95] );
+	synth.triggerAttackRelease(notas, [0.95]);
 }
 
-function GerarAcordeAleatorio()
-{
+function GerarAcordeAleatorio() {
 	var chaves = Object.keys(ACORDES);
-	var indiceAleatorio =   Math.floor(Math.random()*chaves.length);
-	var acordeAleatorio =  chaves[indiceAleatorio];
-	IniciarReproducao(acordeAleatorio );
+	var indiceAleatorio = Math.floor(Math.random() * chaves.length);
+	var acordeAleatorio = chaves[indiceAleatorio];
+	IniciarReproducao(acordeAleatorio);
 }
